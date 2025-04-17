cf739e110a1942b026a34ba695e20e054227c626
مهران صرافی <50288977+MrMehranSarrafi@users.noreply.github.com>
2025-04-17 17:10:56 +0330

    Add files via upload

---
+++ mainShemroonkabab.js
@@ -0,0 +1,9 @@
+﻿let mymodal = document.querySelector("#container2 .mymodal");
+function callModal(){
+	mymodal.style.display="flex";
+}
+function closeModal(){
+	
+	mymodal.style.display="none";
+}
+  
